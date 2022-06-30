d82c979347923ab39d59479c9b608e1944ec35e8
Rony038 <absiddikrony00@gmail.com>
2022-06-30 18:29:38 +0600

    To-Do Website

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'to-do-list';
+  //result:any;
+
+  list:any[]=[];
+  AddTask(item:string){
+    this.list.push({id:this.list.length, name:item});
+    console.warn(this.list);
+  }
+  removeTask(id:number){
+    console.warn(id);
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+}
